c253d4bec8fd391126ec6123d19fc0f94f5bfb8e
TenClay, Ethan <ethantenclay@my.unt.edu>
2026-02-22 01:05:11 -0600

    fix(pi): evolve Pi runtime behavior
    - Using sparse checkout now, updating
    - Needed tailing newline
    - execstart path

--- pi/system/sync-on-boot.sh
+++ pi/system/sync-on-boot.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-cd /home/pi/goat-capture
+cd /home/pi/goatdev
 git fetch origin
 git reset --hard origin/main
 
 source /home/pi/venv/bin/activate
 pip install -r pi/requirements.txt --quiet
 
-sudo bash /home/pi/goat-capture/pi/system/deploy.sh
+sudo bash /home/pi/goatdev/pi/system/deploy.sh
 
 echo "Boot sync completed at $(date)" >> /home/pi/boot-sync.log
 git log -1 --oneline >> /home/pi/boot-sync.log
